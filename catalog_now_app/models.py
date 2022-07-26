f8e72fd1cf6999fcf32d96ee0d0d9903ab8d2eb2
Cesar Costa <ccosta@ca.gob.ar>
2022-07-25 21:10:33 -0300

    Models

---
+++ catalog_now_app/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
+
+class Publisher(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}"
+
+class Product(models.Model):
+    product_title = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=180)
+    description = RichTextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to="articles", null=True, blank=True)
+    author = models.ForeignKey(Publisher, on_delete=models.DO_NOTHING)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_published = models.DateTimeField()
+
+
+    
+class Catalog(models.Model):
+    name = models.CharField(max_length=20)
+    social_network_one = models.URLField(null=True)
+    social_network_two = models.URLField(null=True)
+    email = models.EmailField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
