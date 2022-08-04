239a444b18aee83e9dab2078c3b214e8f0c7fb44
Cesar Costa <ccosta@ca.gob.ar>
2022-08-04 19:12:53 -0300

    ajustes UI base.html, list_prducts.html, about_us.html, index.html, product_detali.html + se muestra usuario logueado

--- panel_app/views.py
+++ panel_app/views.py
@@ -12,6 +12,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 class ProductList(LoginRequiredMixin, ListView):
     queryset = Product.objects.all()
