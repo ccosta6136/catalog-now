0e91b544f8a2ca7bbf7abe52bc0da075da01a94f
Cesar Costa <ccosta@ca.gob.ar>
2022-08-04 00:31:45 -0300

    armado de lista de productos y detalle para el usurio no registrado

--- catalog_now_app/urls.py
+++ catalog_now_app/urls.py
@@ -1,6 +1,7 @@
 from xml.etree.ElementInclude import include
 from django.urls import path, include
-from .views import home, SobreNosotros, MainPageView, ProductDetailView
+from .views import home, AboutUs, MainPageView, ProductDetailView,ProductListUser
+
 
 
 
@@ -8,7 +9,8 @@ from .views import home, SobreNosotros, MainPageView, ProductDetailView
 urlpatterns = [
     path('', home, name="index"),
     path('', MainPageView.as_view(), name='main-page'),
-    path('sobre_nosotros/', SobreNosotros.as_view(), name="sobre-nosotros"),
-    path('product/<pk>/', ProductDetailView.as_view(), name='product-detail'),
+    path('about_us/', AboutUs.as_view(), name="about-us"),
+    path('product/',  ProductListUser.as_view(), name ="product-list" ),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
 
--- catalog_now_app/views.py
+++ catalog_now_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView, View
-from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from .models import Product, Catalog
+
 # Create your views here.
 
 def home(request):
@@ -14,17 +14,20 @@ class BaseView(View):
         context['catalog'] = Catalog.objects.order_by('date_updated').first()
         return context    
 
-class SobreNosotros(BaseView, TemplateView):
-    template_name = "catalog_now_app/sobre_nosotros.html"        
+class AboutUs(BaseView, TemplateView):
+    template_name = "catalog_now_app/about_us.html"        
 
 class MainPageView(BaseView):
     template_name = "catalog_now_app/index.html"   
 
 class ProductDetailView(DetailView):
     model = Product
-    context_object_name = "product"
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['catalog'] = Catalog.objects.order_by('date_updated').first()
-        return context
+    template_name = "catalog_now_app/product_detail.html"  
+   
+
+class ProductListUser(ListView):
+    model = Product
+    template_name = "catalog_now_app/list_products.html"
+    context_object_name = "products"
+  
+     
