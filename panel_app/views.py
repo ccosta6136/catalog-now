0312849035013dbfa51829d12b4694d1e0be4b06
Pablo Moreno <pomoreno@ca.gob.ar>
2022-08-05 11:30:07 -0300

    Adding baseview

--- panel_app/views.py
+++ panel_app/views.py
@@ -25,19 +25,19 @@ class ProductDetail(LoginRequiredMixin, BaseView, DetailView):
     model = Product 
     template_name = "catalog_now_app/product_detalle.html"
 
-class ProductCreate(LoginRequiredMixin, CreateView):
+class ProductCreate(LoginRequiredMixin, BaseView, CreateView):
     model = Product
     success_url = reverse_lazy("panel-page")
     fields = ['product_title','short_description','description','price','image','author','is_headline','date_published']
     permission_required = ("catalog_now_app.add_product")
 
-class ProductUpdate(LoginRequiredMixin, UpdateView):
+class ProductUpdate(LoginRequiredMixin, BaseView, UpdateView):
     model = Product
     success_url = reverse_lazy("panel-page")
     fields = ['product_title','short_description','description','price','image','author','is_headline','date_published']
     permission_required = ("catalog_now_app.change_product")
 
-class ProductDelete(LoginRequiredMixin, DeleteView):
+class ProductDelete(LoginRequiredMixin, BaseView, DeleteView):
     model = Product
     success_url = reverse_lazy("panel-page")
     permission_required = ("catalog_now_app.delete_product")
@@ -46,7 +46,7 @@ class PanelLogin(LoginView, BaseView):
     template_name = 'catalog_now_app/panel_login.html'
     next_page = reverse_lazy("panel-page")
 
-class PanelLogout(LogoutView):
+class PanelLogout(LogoutView,  BaseView,):
     template_name = 'catalog_now_app/panel_logout.html'
 
 class SignUpView(SuccessMessageMixin, BaseView, CreateView):
@@ -55,7 +55,7 @@ class SignUpView(SuccessMessageMixin, BaseView, CreateView):
     form_class = UserCreationForm
     success_message = "¡¡ Se creo tu perfil satisfactoriamente !!"
 
-class UserProfile(LoginRequiredMixin, UserPassesTestMixin, DetailView):
+class UserProfile(LoginRequiredMixin, UserPassesTestMixin,  BaseView, DetailView):
     model = User
     template_name = "user_profile/user_detail.html"
 
@@ -73,17 +73,17 @@ class UserUpdate(LoginRequiredMixin, UserPassesTestMixin, BaseView, UpdateView):
     def test_func(self):
         return self.request.user.id == int(self.kwargs['pk'])
 
-class CatalogList(LoginRequiredMixin, ListView):
+class CatalogList(LoginRequiredMixin, BaseView, ListView):
     queryset = Catalog.objects.all()
     template_name = "catalog_now_app/catalog_list.html"
     context_object_name = "catalogs"
 
-class CatalogCreate(LoginRequiredMixin, CreateView):
+class CatalogCreate(LoginRequiredMixin, BaseView, CreateView):
     model = Catalog
     success_url = reverse_lazy("catalog-page")
     fields = ['name','social_network_one','social_network_two','email']
 
-class CatalogUpdate(LoginRequiredMixin, UpdateView):
+class CatalogUpdate(LoginRequiredMixin, BaseView, UpdateView):
     model = Catalog
     success_url = reverse_lazy("catalog-page")
     fields = ['name','social_network_one','social_network_two','email']
